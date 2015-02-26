63d9f8926ee287a8a732f2281ff9ccc8295775aa
Kevin OBrien <kevin.obrien@praece.com>
2015-02-26 00:03:34 -0800

    Made date inputs not look disabled, changed pickadate version.

--- src/directives/datepicker.js
+++ src/directives/datepicker.js
@@ -8,18 +8,36 @@ function() {
     restrict: 'A',
     link: function(scope, element, attr, ctrl) {
       element.pickadate({
-        format: 'm/d/yy'
+        format: 'm/d/yy',
+        editable: true,
+        selectYears: true,
+        selectMonths: true
       });
+
+      var picker = element.pickadate('picker');
 
       ctrl.$formatters.unshift(function(modelValue) {
         if (!modelValue) {
           return null;
         }
 
-        return moment(modelValue).format('M/D/YY');
+        var date = moment(modelValue);
+
+        if (date.isValid()) {
+          picker.set('highlight', date.toDate());
+        }
+
+        return date.format('M/D/YY');
       });
+
       ctrl.$parsers.unshift(function(viewValue) {
-        return moment(viewValue).format('YYYY/MM/DD');
+        var date = moment(viewValue, 'M/D/YY', true);
+
+        if (date.isValid()) {
+          picker.set('highlight', date.toDate());
+        }
+
+        return date.toDate();
       });
     }
   };
@@ -29,10 +47,27 @@ angular.module('pr.forms').directive('prTimePicker', [
 
 function() {
   return {
-    link: function(scope, element, attr) {
+    require: 'ngModel',
+    restrict: 'A',
+    link: function(scope, element, attr, ctrl) {
       element.pickatime({
         min: [5,0],
-        max: [20,0]
+        max: [20,0],
+        editable: true
+      });
+
+      var picker = element.pickatime('picker');
+
+      ctrl.$formatters.unshift(function(modelValue) {
+        picker.set('highlight', modelValue);
+
+        return modelValue;
+      });
+
+      ctrl.$parsers.unshift(function(viewValue) {
+        picker.set('highlight', viewValue);
+
+        return viewValue;
       });
     }
   };
